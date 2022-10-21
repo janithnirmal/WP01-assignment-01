76ec0af3a4f42722c71934a2e980d672ac0e1007
janithnirmal <113751270+janithnirmal@users.noreply.github.com>
2022-10-21 13:15:30 +0530

    basic project environment created

---
+++ index.php
@@ -0,0 +1,22 @@
+<!-- log in page -->
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Shop | Log In</title>
+
+    <!-- css files -->
+    <link rel="stylesheet" href="css/style.css" />
+</head>
+
+<body>
+
+
+
+    <script src="scripts/scripts.js"></script>
+</body>
+
+</html>
