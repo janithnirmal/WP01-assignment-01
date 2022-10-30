5ef4051fd759073e2c37fef77163a0939b8254f0
janithnirmal <113751270+janithnirmal@users.noreply.github.com>
2022-10-31 00:57:38 +0530

    footer added, bug fixed
    container bug fixed,
    footer section added, designed, makeit responsive as wel

---
+++ pages/cart.php
@@ -0,0 +1,233 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>New Tech</title>
+    <link rel="icon" href="../resources/logo/icon.svg" />
+
+    <!-- css files -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="../css/bootstrap.css" />
+    <link rel="stylesheet" href="../css/style.css" />
+</head>
+
+<body>
+
+    <!-- nav bar -->
+    <header class="profile-header">
+        <!-- navbar -->
+        <div class="col12 h-100">
+            <div class="container h-100">
+                <div class="row h-100 align-items-center justify-content-center">
+                    <div class="col-3 ">
+                        <a href="home.php">
+                            <div class="back-to-home fs-4 fw-bold"><i class="bi bi-arrow-left"></i> To Home</div>
+                        </a>
+                    </div>
+
+                    <div class="col-6 fw-bold fs-3 text-center">Product Cart</div>
+
+                    <div class="col-3 d-flex align-items-center ">
+                        <!-- purches details -->
+                        <div class="d-flex justify-content-center align-items-center">
+                        <a href="bookmark.php"><i class="bi bi-bookmark text-white" style="font-size: 22px;"></i></a>
+                            <i class="bi ms-1 bi-record-fill text-white" style="font-size: 8px;"></i>
+                            <a href="profile.php"><i class="bi ms-1 bi-person-circle text-white" style="font-size: 22px;"></i></a>
+                            <i class="bi ms-1 bi-record-fill text-white d-md-flex me-1 d-none" style="font-size: 8px;"></i>
+                        </div>
+
+                        <!-- user details / log in details -->
+                        <div class="profile-details flex-column ms-1 d-md-flex d-none border-start border-2 ps-2">
+                            <span class="fw-bold text-white" style="font-size: 13px;">Janith Nirmal</span>
+                            <span class="text-white" style="font-size: 13px;">rmjanithnirmal@gmail.com</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+
+
+    <!-- content -->
+    <section>
+        <div class="col-12">
+            <div class="container">
+                <div class="row p-3 d-flex justify-content-between">
+
+                    <div class="col-12 col-lg-8">
+                        <div class="col-12  subContent-container pb-2 profileContentBox">
+                            <div class="container px-3">
+                                <div class="col-12 d-flex px-4 justify-content-between">
+                                    <div class="row pt-3 fs-6 fw-bold">Product Details</div>
+                                    <div class="row pt-3 fs-6 fw-bold">Availability</div>
+                                    <div class="row pt-3 fs-6 fw-bold">Price</div>
+                                </div>
+
+                                <div class="col-12 px-2">
+                                    <hr />
+                                </div>
+
+                                <div class="col-12">
+                                    <div class="bg-white col-12 p-1 pb-3 px-4 my-3">
+                                        <div class="row pt-3">
+                                            <div class="col-6 fw-bold">Title of the product</div>
+                                            <div class="col-4">Available</div>
+                                            <div class="col-2 d-flex justify-content-end">$500.00</div>
+                                        </div>
+
+
+                                        <div class="col-12 ">
+                                            <hr />
+                                        </div>
+
+                                        <div class="row mt-3">
+                                            <div class="col-4">
+                                                <div class="product-img" style="width: 150px; height: 150px; background-image: url('../resources/images/c2.jpg'); background-size: contain; background-repeat: none; "></div>
+                                            </div>
+
+                                            <div class="col-8">
+                                                <div class="row">
+                                                    <p class="h-75">Donerton Wireless Earbud, Bluetooth 5.3 Headphones Noise Cancelling Mic, Bluetooth Earbud Bass Stereo, 50 hrs Playtime LED Display Earphones, IP7 Waterproof Over Ear Buds for Sport</p>
+                                                    <div class="col-12">
+                                                        <div class="row">
+                                                            <div class="col-4">
+                                                                <button class="text-dark c-btn" style="background-color: transparent; border: 1px rgba(0, 0, 0, 0.589) solid;">Remove</button>
+                                                            </div>
+                                                            <div class="col-4">
+                                                                <button class="c-btn bg-secondary">Add to Cart</button>
+                                                            </div>
+                                                            <div class="col-4">
+                                                                <button class="c-btn bg-danger">Buy</button>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="bg-white col-12 p-1 pb-3 px-4 my-3">
+                                        <div class="row pt-3">
+                                            <div class="col-6 fw-bold">Title of the product</div>
+                                            <div class="col-4">Available</div>
+                                            <div class="col-2 d-flex justify-content-end">$500.00</div>
+                                        </div>
+
+
+                                        <div class="col-12 ">
+                                            <hr />
+                                        </div>
+
+                                        <div class="row mt-3">
+                                            <div class="col-4">
+                                                <div class="product-img" style="width: 150px; height: 150px; background-image: url('../resources/images/c2.jpg'); background-size: contain; background-repeat: none; "></div>
+                                            </div>
+
+                                            <div class="col-8">
+                                                <div class="row">
+                                                    <p class="h-75">Donerton Wireless Earbud, Bluetooth 5.3 Headphones Noise Cancelling Mic, Bluetooth Earbud Bass Stereo, 50 hrs Playtime LED Display Earphones, IP7 Waterproof Over Ear Buds for Sport</p>
+                                                    <div class="col-12">
+                                                        <div class="row">
+                                                            <div class="col-4">
+                                                                <button class="text-dark c-btn" style="background-color: transparent; border: 1px rgba(0, 0, 0, 0.589) solid;">Remove</button>
+                                                            </div>
+                                                            <div class="col-4">
+                                                                <button class="c-btn bg-secondary">Add to Cart</button>
+                                                            </div>
+                                                            <div class="col-4">
+                                                                <button class="c-btn bg-danger">Buy</button>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="bg-white col-12 p-1 pb-3 px-4 my-3">
+                                        <div class="row pt-3">
+                                            <div class="col-6 fw-bold">Title of the product</div>
+                                            <div class="col-4">Available</div>
+                                            <div class="col-2 d-flex justify-content-end">$500.00</div>
+                                        </div>
+
+
+                                        <div class="col-12 ">
+                                            <hr />
+                                        </div>
+
+                                        <div class="row mt-3">
+                                            <div class="col-4">
+                                                <div class="product-img" style="width: 150px; height: 150px; background-image: url('../resources/images/c2.jpg'); background-size: contain; background-repeat: none; "></div>
+                                            </div>
+
+                                            <div class="col-8">
+                                                <div class="row">
+                                                    <p class="h-75">Donerton Wireless Earbud, Bluetooth 5.3 Headphones Noise Cancelling Mic, Bluetooth Earbud Bass Stereo, 50 hrs Playtime LED Display Earphones, IP7 Waterproof Over Ear Buds for Sport</p>
+                                                    <div class="col-12">
+                                                        <div class="row">
+                                                            <div class="col-4">
+                                                                <button class="text-dark c-btn" style="background-color: transparent; border: 1px rgba(0, 0, 0, 0.589) solid;">Remove</button>
+                                                            </div>
+                                                            <div class="col-4">
+                                                                <button class="c-btn bg-secondary">Add to Cart</button>
+                                                            </div>
+                                                            <div class="col-4">
+                                                                <button class="c-btn bg-danger">Buy</button>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+
+
+
+                        </div>
+                    </div>
+
+                    <div class="col-12 col-lg-4 bg-dark">
+                        <div class="col-12 border-1 h-100">
+                            <div class="col-12">
+                                <span class="row mt-2 mb-2 justify-content-center text-light fs-6 fw-bold">Paid Promotions</span>
+                                <div class="row">
+                                    <hr class="border-light" />
+
+                                    <!-- below code will repeat for each paid promotion content -->
+                                    <!-- paid promotion goes here -->
+                                    <div class="col-12 text-light">
+                                        <!-- add the adverticement -->
+                                    </div>
+                                </div>
+                            </div>
+
+
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        </div>
+    </section>
+
+
+
+
+    <!-- footer -->
+    <?php include("footer.php"); ?>
+
+    <script src="../scripts/bootstrap.bundle.js"></script>
+    <script src="../scripts/scripts.js"></script>
+</body>
+
+</html>
